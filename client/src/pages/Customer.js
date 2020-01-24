0c229592c8540d88b41af544cb57a850156284a8
Raleigh <53280832+jkoenig19@users.noreply.github.com>
2020-01-23 21:49:29 -0500

    Update customer.js and form component to fix dropdowns

--- client/src/pages/Customer.js
+++ client/src/pages/Customer.js
@@ -1,6 +1,6 @@
  import React , { Component } from "react";
  import './style.css';
- import { Input, TextArea, FormBtn, Option, SelectBox} from "../components/Form";
+ import { Input, TextArea, FormBtn } from "../components/Form";
  
  
 
@@ -11,107 +11,122 @@
       writingOnCake: "",
       decorations: "",
       cookieQuantity:"",
-      cupcakeQuantity:""
+      cupcakeQuantity:"",
+      valueFlavor: "",
+      valueSize: "",
+      valueOutside: "",
+      valueInside: ""
     };
+
+    handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({
+        [name]: value
+      });
+    };
+
+    handleFormSubmit = event => {
+      event.preventDefault();
+    }
 
     render() {
         return (
             <div className= "container-fluid">
- <form>
- <Input
-   value={this.state.customerName}
-   onChange={this.handleInputChange}
-   name="customerName"
-   placeholder="Name"
- />
- <Input
-   value={this.state.dateOfPickup}
-   onChange={this.handleInputChange}
-   name="dateOfPickup"
-   placeholder="Date of Pickup"
- />
- <p>Cake Flavor</p>
- <SelectBox onChange ={this.handleChange} value={this.state.value}>
-     <Option value ="vanilla">Vanilla</Option>
-     <Option value ="chocolate">Chocolate</Option>
-     <Option value ="carrot">Carrot</Option>
-     <Option value ="redVelvet">Red Velvet</Option>
-   </SelectBox>
- <p>Cake Size</p>
- <SelectBox onChange ={this.handleChange} value={this.state.value}>
-     <Option value ="6inch">6 inch</Option>
-     <Option value ="9inch">9 inch</Option>
-     <Option value ="12inch">12 inch</Option>
-     <Option value ="14inch">14 inch</Option>
-     <Option value ="fourthsheet">1/4 Sheet</Option>
-     <Option value ="halfsheet">1/2 Sheet</Option>
- </SelectBox>
- <p>Butter Cream Outside</p>
- <SelectBox onChange ={this.handleChange} value={this.state.value}>
-     <Option value ="vanilla">Vanilla</Option>
-     <Option value ="almond">Almond</Option>
-     <Option value ="blackberry">Blackberry</Option>
-     <Option value ="chocolate">Chocolate</Option>
-     <Option value ="mocha">Mocha</Option>
-     <Option value ="mint">Mint</Option>
-     <Option value ="strawberry">Strawberry</Option>
-     <Option value ="caramel">Caramel</Option>
-     <Option value ="peanutButter">Peanut Butter</Option>
-     <Option value ="raspberry">Reaspberry</Option>
-     <Option value ="blueberry">Blueberry</Option>
-     <Option value ="creamCheese">Cream Cheese</Option>
-     <Option value ="ganache">Ganache</Option>
- </SelectBox>
- <p>Butter Cream Inside</p>
- <SelectBox onChange ={this.handleChange} value={this.state.value}>
-     <Option value ="vanilla">Vanilla</Option>
-     <Option value ="almond">Almond</Option>
-     <Option value ="blackberry">Blackberry</Option>
-     <Option value ="chocolate">Chocolate</Option>
-     <Option value ="mocha">Mocha</Option>
-     <Option value ="mint">Mint</Option>
-     <Option value ="strawberry">Strawberry</Option>
-     <Option value ="caramel">Caramel</Option>
-     <Option value ="peanutButter">Peanut Butter</Option>
-     <Option value ="raspberry">Reaspberry</Option>
-     <Option value ="blueberry">Blueberry</Option>
-     <Option value ="creamCheese">Cream Cheese</Option>
-     <Option value ="ganache">Ganache</Option>
- </SelectBox>
- <TextArea
-   value={this.state.writingOnCake}
-   onChange={this.handleInputChange}
-   name="writingOnCake"
-   placeholder=""
- />
-  <TextArea
-   value={this.state.decorations}
-   onChange={this.handleInputChange}
-   name="decorations"
-   placeholder="decoration"
- />
-  <Input
-   value={this.state.cookieQuantity}
-   onChange={this.handleInputChange}
-   name="cookieQuantity"
-   placeholder="How many cookies would you like"
- />
-   <Input
-   value={this.state.cupcakeQuantity}
-   onChange={this.handleInputChange}
-   name="cupcakeQuantity"
-   placeholder="How many cupcakes would you like"
- />
- <FormBtn
-   
-   onClick={this.handleFormSubmit}
- >
-   Submit Order
- </FormBtn>
-</form>
-</div>
-        )}
- }
+              <form>
+              <Input
+                value={this.state.customerName}
+                onChange={this.handleInputChange}
+                name="customerName"
+                placeholder="Name"
+              />
+              <Input
+                value={this.state.dateOfPickup}
+                onChange={this.handleInputChange}
+                name="dateOfPickup"
+                placeholder="Date of Pickup"
+              />
+              <p>Cake Flavor</p>
+              <select className="form-control" name="valueFlavor" onChange ={this.handleInputChange} value={this.state.valueFlavor}>
+                  <option value ="vanilla">Vanilla</option>
+                  <option value ="chocolate">Chocolate</option>
+                  <option value ="carrot">Carrot</option>
+                  <option value ="redVelvet">Red Velvet</option>
+              </select>
+              <p>Cake Size</p>
+              <select className="form-control" name="valueSize" onChange ={this.handleInputChange} value={this.state.valueSize}>
+                  <option value ="6inch">6 inch</option>
+                  <option value ="9inch">9 inch</option>
+                  <option value ="12inch">12 inch</option>
+                  <option value ="14inch">14 inch</option>
+                  <option value ="fourthsheet">1/4 Sheet</option>
+                  <option value ="halfsheet">1/2 Sheet</option>
+              </select>
+              <p>Butter Cream Outside</p>
+              <select className="form-control" name="valueOutside" onChange ={this.handleInputChange} value={this.state.valueOutside}>
+                  <option value ="vanilla">Vanilla</option>
+                  <option value ="almond">Almond</option>
+                  <option value ="blackberry">Blackberry</option>
+                  <option value ="chocolate">Chocolate</option>
+                  <option value ="mocha">Mocha</option>
+                  <option value ="mint">Mint</option>
+                  <option value ="strawberry">Strawberry</option>
+                  <option value ="caramel">Caramel</option>
+                  <option value ="peanutButter">Peanut Butter</option>
+                  <option value ="raspberry">Reaspberry</option>
+                  <option value ="blueberry">Blueberry</option>
+                  <option value ="creamCheese">Cream Cheese</option>
+                  <option value ="ganache">Ganache</option>
+              </select>
+              <p>Butter Cream Inside</p>
+              <select className="form-control" name="valueInside" onChange ={this.handleInputChange} value={this.state.valueInside}>
+                  <option value ="vanilla">Vanilla</option>
+                  <option value ="almond">Almond</option>
+                  <option value ="blackberry">Blackberry</option>
+                  <option value ="chocolate">Chocolate</option>
+                  <option value ="mocha">Mocha</option>
+                  <option value ="mint">Mint</option>
+                  <option value ="strawberry">Strawberry</option>
+                  <option value ="caramel">Caramel</option>
+                  <option value ="peanutButter">Peanut Butter</option>
+                  <option value ="raspberry">Reaspberry</option>
+                  <option value ="blueberry">Blueberry</option>
+                  <option value ="creamCheese">Cream Cheese</option>
+                  <option value ="ganache">Ganache</option>
+              </select>
+              <TextArea
+                value={this.state.writingOnCake}
+                onChange={this.handleInputChange}
+                name="writingOnCake"
+                placeholder=""
+              />
+                <TextArea
+                value={this.state.decorations}
+                onChange={this.handleInputChange}
+                name="decorations"
+                placeholder="decoration"
+              />
+                <Input
+                value={this.state.cookieQuantity}
+                onChange={this.handleInputChange}
+                name="cookieQuantity"
+                placeholder="How many cookies would you like"
+              />
+                <Input
+                value={this.state.cupcakeQuantity}
+                onChange={this.handleInputChange}
+                name="cupcakeQuantity"
+                placeholder="How many cupcakes would you like"
+              />
+              <FormBtn
+                onClick={this.handleFormSubmit}
+              >
+                Submit Order
+              </FormBtn>
+              </form>
+            </div>
+        )
+    }
+  }
 
 
  export default Customer;
