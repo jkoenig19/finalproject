6dcb3ba6be87116c3042f901352ec0675293e565
Annemarie Hin <ame5033@gmail.com>
2020-01-28 21:55:50 -0500

    updates to customer, bakery and home pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,27 +16,47 @@ class Home extends Component {
   render() {
     return (
       <div className="bg">
-<div className="top">
-        <nav className="navbar navbar-expand-lg ">
-        <i className="material-icons">cake</i> <br></br><a class="navbar-brand" href="#">Bakery Link</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="#">Customer Log In <span class="sr-only">(current)</span></a>
-              <a className="nav-item nav-link active" href="#">Bakery Log In</a>
+        <div className="top">
+          <nav className="navbar navbar-expand-lg ">
+            <i className="material-icons">cake</i> <br></br><a class="navbar-brand" href="#">Bakery Link</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-item nav-link active" href="#">Customer Log In <span class="sr-only">(current)</span></a>
+                <a className="nav-item nav-link active" href="#">Bakery Log In</a>
+              </div>
             </div>
-          </div>
-        </nav>
-</div>
-
-
+          </nav>
+        </div>
         <Jumbotron>
           <h1 className="display-4">Bakery Link</h1>
           <h2 className="lead">Making Bakery Orders A Piece Of Cake!</h2>
         </Jumbotron>
+      <div className="signUp">
+        <button className= "bttnOne" className="btn btn-primary" type="submit">New Customer Sign Up</button>    <button className="bttnTwo" className="btn btn-primary" type="submit">New Bakery Sign Up</button>
+</div>
+<div className="text">
+        <div className="customer">
+          <img className="demoimage" src={require("../images/hotfudge.jpg")} width="150px" height="135px" />
+          <p className="demoText">  Use Bakery Link to place your bakery orders online and come back to check order status.</p>
 
+        </div>
+    <br></br>
+    
+       <hr></hr>
+    
+
+        <div className="bakery">
+        <img className="bakeryImage" src={require("../images/four-macaroons-808941.jpg")} width="150px" height="150px" />
+        <p className="bakeryText">Bakeries can use Bakery Link to receive online orders from customers, track orders and manage inventory.</p>
+  
+        </div>
+        </div>
+<br></br>
+<br></br>
+<br></br>
         <Footer></Footer>
       </div>
 
